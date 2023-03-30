5dd226f00ed961a250aad6eff3bd60c1b7b4c960
Huiping Guo <huiping192@gmail.com>
2023-03-30 13:11:36 +0900

    Add Test class and related logic

--- Sources/commitlogGPT/main.swift
+++ Sources/commitlogGPT/main.swift
@@ -9,7 +9,7 @@ import Foundation
 
 let openAIRepository = OpenAIRepository()
 let keyManager = KeyManager()
-
+let test = Test()
 
 let cachedToken = keyManager.receiveCachedToken()
 var openAIToken: String = ""
@@ -30,7 +30,7 @@ if let cachedToken {
 
 openAIRepository.configureToken(token: openAIToken)
 
-let diff = runShellCommand("/usr/bin/git", arguments: ["diff", "--cached"])
+let diff = test.runShellCommand("/usr/bin/git", arguments: ["diff", "--cached"])
 
 if diff.isEmpty {
   print("No changes to commit.")
@@ -40,83 +40,86 @@ if diff.isEmpty {
 do {
   print("Requesting commit logs...")
   let commitlogs = try await openAIRepository.receiveCommitlogs(by: diff)
-  await confirmCommitLog(suggestCommitLogs: commitlogs)
+  await test.confirmCommitLog(suggestCommitLogs: commitlogs)
 } catch {
   print("Failed to get result from openapi. error: \(error.localizedDescription)")
 }
   
 
 
-@Sendable func runShellCommand(_ command: String, arguments: [String]) -> String {
-  let process = Process()
-  process.executableURL = URL(fileURLWithPath: command)
-  process.arguments = arguments
-  
-  let outputPipe = Pipe()
-  process.standardOutput = outputPipe
-  
-  do {
-    try process.run()
-  } catch {
-    print("Failed to run \(command) \(arguments.joined(separator: " "))")
-    exit(1)
-  }
-  
-  let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-  return String(data: outputData, encoding: .utf8) ?? ""
-}
 
-
-func escapeCommitMessage(_ message: String) -> String {
-  return message.replacingOccurrences(of: "'", with: "\\'")
-}
-
-func promptUser(choices: [String]) -> String? {  
-  for (index, choice) in choices.enumerated() {
-    print("\(index + 1). \(choice)")
-  }
-  print("Enter your choice number:")
-  if let input = readLine(), let choice = Int(input), 1...choices.count ~= choice {
-    return choices[choice - 1]
-  }
-  return nil
-}
-
-
-func confirmCommitLog(suggestCommitLogs: [String]) async {
-  let more = "More Options"
-  let quit = "Quit"
-  
-  var choices: [String] = suggestCommitLogs
-  choices.append(more)
-  choices.append(quit)
-
-  while true {
-    guard let answer = promptUser(choices: choices) else {
-      print("Invalid choice. Please try again.")
-      continue
+class Test {
+  @Sendable func runShellCommand(_ command: String, arguments: [String]) -> String {
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: command)
+    process.arguments = arguments
+    
+    let outputPipe = Pipe()
+    process.standardOutput = outputPipe
+    
+    do {
+      try process.run()
+    } catch {
+      print("Failed to run \(command) \(arguments.joined(separator: " "))")
+      exit(1)
     }
-        
-    if answer == more {
-      await getMore()
-      return
-    } else if answer == quit {
-      exit(0)
-    } else {
-      let escapedMessage = escapeCommitMessage(answer)
-      let _ = runShellCommand("/usr/bin/git", arguments: ["commit", "-m", escapedMessage])
-      return
+    
+    let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+    return String(data: outputData, encoding: .utf8) ?? ""
+  }
+
+
+  func escapeCommitMessage(_ message: String) -> String {
+    return message.replacingOccurrences(of: "'", with: "\\'")
+  }
+
+  func promptUser(choices: [String]) -> String? {
+    for (index, choice) in choices.enumerated() {
+      print("\(index + 1). \(choice)")
+    }
+    print("Enter your choice number:")
+    if let input = readLine(), let choice = Int(input), 1...choices.count ~= choice {
+      return choices[choice - 1]
+    }
+    return nil
+  }
+
+
+  func confirmCommitLog(suggestCommitLogs: [String]) async {
+    let more = "More Options"
+    let quit = "Quit"
+    
+    var choices: [String] = suggestCommitLogs
+    choices.append(more)
+    choices.append(quit)
+
+    while true {
+      guard let answer = promptUser(choices: choices) else {
+        print("Invalid choice. Please try again.")
+        continue
+      }
+          
+      if answer == more {
+        await getMore()
+        return
+      } else if answer == quit {
+        exit(0)
+      } else {
+        let escapedMessage = escapeCommitMessage(answer)
+        let _ = runShellCommand("/usr/bin/git", arguments: ["commit", "-m", escapedMessage])
+        return
+      }
     }
   }
-}
 
 
-func getMore() async {
-  do {
-    print("Requesting more commit logs...")
-    let commitlogs = try await openAIRepository.receiveCommitlogs(by: diff)
-    await confirmCommitLog(suggestCommitLogs: commitlogs)
-  } catch {
-    print("Failed to get result from openapi. error: \(error.localizedDescription)")
+  func getMore() async {
+    do {
+      print("Requesting more commit logs...")
+      let commitlogs = try await openAIRepository.receiveCommitlogs(by: diff)
+      await confirmCommitLog(suggestCommitLogs: commitlogs)
+    } catch {
+      print("Failed to get result from openapi. error: \(error.localizedDescription)")
+    }
   }
 }
